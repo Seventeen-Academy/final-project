f12d6a36d5ca5036d5017ff6bce20afdea4768ee
gemakurnia <kurniagema@gmail.com>
2023-06-02 14:48:20 +0700

    first commit

---
+++ src/pages/Form.jsx
@@ -0,0 +1,12 @@
+import "../assets/css/style-laporan.css";
+import Navbar from "../components/GlobalComponent/Navbar";
+import Footer from "../components/GlobalComponent/Footer";
+
+const Form = () => {
+    return (
+        <>
+        <Navbar />
+        <Footer />
+        </>
+    );
+};
