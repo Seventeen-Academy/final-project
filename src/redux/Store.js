9280836b6a274a7963ba4cdbed8a7421546d5dae
Tasya Adila Putri <tasyaadila03@gmail.com>
2023-06-14 09:09:17 +0700

    import articleReducer at store

--- src/redux/Store.js
+++ src/redux/Store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./reducers/User";
+import articleReducer from "./reducers/Article";
 
 export default configureStore({
   reducer: {
     userReducer,
+    articleReducer,
   },
 });
